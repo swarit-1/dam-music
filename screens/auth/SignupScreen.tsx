353982bac5b9f267ac105432b902fb3116174768
joshuawu <joshwu10806@gmail.com>
2025-10-27 17:55:48 -0500

    change stuff

--- screens/auth/SignupScreen.tsx
+++ screens/auth/SignupScreen.tsx
@@ -13,7 +13,7 @@ import {
 } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { signUp } from '../../src/services/authService';
-import
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 export default function SignupScreen({ navigation }: any) {
   const [name, setName] = useState('');
